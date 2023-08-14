985e51c388cbce73ae8bc2ba63e44a8b847d7d1a
Tuhinadri Banerjee <tuhiniris@gmail.com>
2023-08-14 19:15:36 +0530

    Added Fresh Bot Code

--- ysrsmartbot/src/App.js
+++ ysrsmartbot/src/App.js
@@ -3,15 +3,33 @@ import ActionProvider from "./ActionProvider";
 import config from "./config";
 import "./App.css";
 import Chatbot from "react-chatbot-kit";
-import "react-chatbot-kit/build/main.css";
+import "./design.css";
+//import { useState } from "react";
 
 function App() {
+  //const [showBot, toggleBot] = useState(false);
+
+  const saveMessages = (messages, HTMLString) => {
+    localStorage.setItem("chat_messages", JSON.stringify(messages));
+  };
+
+  const loadMessages = () => {
+    const messages = JSON.parse(localStorage.getItem("chat_messages"));
+    return messages;
+  };
+
   return (
     <div>
       <Chatbot
-        config={config}
-        messageParser={MessageParser}
         actionProvider={ActionProvider}
+        messageParser={MessageParser}
+        config={config}
+        headerText="Conversation with YSR Chatbot"
+        placeholderText="Write your message here"
+        messageHistory={loadMessages()}
+        saveMessages={saveMessages}
+        runInitialMessagesWithHistory
+        disableScrollToBottom
       />
     </div>
   );
--- ysrsmartbot/src/config.js
+++ ysrsmartbot/src/config.js
@@ -1,7 +1,21 @@
+// in config.js
 import { createChatBotMessage } from "react-chatbot-kit";
 
+const botName = "Happy Bot";
+
 const config = {
-  initialMessages: [createChatBotMessage(`Hello world`)]
+  initialMessages: [createChatBotMessage(`Hi! I'm ${botName}`)],
+
+  botName: botName,
+
+  customStyles: {
+    botMessageBox: {
+      backgroundColor: "#376B7E"
+    },
+    chatButton: {
+      backgroundColor: "#5ccc9d"
+    }
+  }
 };
 
 export default config;
